d391ffdaab001aaef6f1de8c83c777a0ad19a835
gabhishekrao <gabhishekraoo@gmail.com>
2024-03-04 17:49:18 +0530

    Completed authentication feature and started implementing Message feature

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import env from "dotenv/config";
-import authRoutes from "./routes/auth.js";
+import authRoutes from "./routes/authRoute.js";
+import messageRoutes from "./routes/messageRoute.js";
 import dbConnection from "./db/dbConnection.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   dbConnection();
