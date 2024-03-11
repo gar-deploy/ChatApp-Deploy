7e8272b8da12937c94918fada8ea901a1966cad4
gabhishekrao <gabhishekraoo@gmail.com>
2024-03-11 15:50:04 +0530

    Initiated frontend, created Login and Signup interface.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/Signup'
+
+function App() {
+
+
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <SignUp/>
+    </div>
+  )
+}
+
+export default App
