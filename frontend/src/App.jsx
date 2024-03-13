9477e3e915305b66505a664ecc8cfd5476c46bad
gabhishekrao <gabhishekraoo@gmail.com>
2024-03-13 18:47:50 +0530

    Created Home page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/Signup'
 
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <SignUp/>
+      <Home/>
     </div>
   )
 }
