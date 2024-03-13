0accbe4194c551cca8e73b9004c0f196504e08b9
gabhishekrao <gabhishekraoo@gmail.com>
2024-03-14 00:05:27 +0530

    Completion of UI

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,16 @@
+
+
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img src="https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg?w=740&t=st=1710353445~exp=1710354045~hmac=9b70607916dc354fe94dc46d0e1e205245a5829eb6de761003b6cf465ae04af9" />
+        </div>
+      </div>
+      <div className="chat-bubble text-white bg-blue-500">Hey! Who are you?</div>
+    </div>
+  );
+};
+
+export default Message;
